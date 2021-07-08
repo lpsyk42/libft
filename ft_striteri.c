f5b12084225bad321c71b11d926f0c0c8e35728e
Lukas Psyk <lpsyk@2-I-3.42heilbronn.de>
2021-07-08 10:51:50 +0200

    adding updated iteri

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: lpsyk <lpsyk@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/07 10:57:15 by lpsyk             #+#    #+#             */
-/*   Updated: 2021/07/07 11:42:00 by lpsyk            ###   ########.fr       */
+/*   Updated: 2021/07/08 10:42:45 by lpsyk            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,15 @@
 void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 {
 	unsigned int	i;
+	unsigned int	s_len;
 
 	if (!s || !f)
 		return ;
+	s_len = ft_strlen(s);
 	i = 0;
-	while (s[i] != '\0')
+	while (i < s_len)
 	{
-		f(i, s + i);
+		f(i, &s[i]);
 		i++;
 	}
 }
